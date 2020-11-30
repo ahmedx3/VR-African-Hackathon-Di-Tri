41dd1d0f6eea28adc13e52a75e6e95c2a4c24f48
bassel97 <bassel.ahmed97@eng-st.cu.edu.eg>
2020-11-30 19:24:11 +0200

    Build ready version 1

---
+++ African Hackathon VR/Assets/Project/Scripts/RestartScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
